04a6ffc7fb6c09bd33530e9a680a24bfcc4201d4
rahul p <rahulpyd@gmail.com>
2017-06-11 05:15:40 +0530

    .

---
+++ public/javascripts/main.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+
+	// Smooth scroll
+	$('a[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			}
+		}
+	});
+
+	$("#portfolio-contant-active").mixItUp();
+
+
+	$("#testimonial-slider").owlCarousel({
+	    paginationSpeed : 500,
+	    singleItem:true,
+	    autoPlay: 3000,
+	});
+
+
+
+
+	$("#clients-logo").owlCarousel({
+		autoPlay: 3000,
+		items : 5,
+		itemsDesktop : [1199,5],
+		itemsDesktopSmall : [979,5],
+	});
+
+	$("#works-logo").owlCarousel({
+		autoPlay: 3000,
+		items : 5,
+		itemsDesktop : [1199,5],
+		itemsDesktopSmall : [979,5],
+	});
+
+
+	// google map
+		var map;
+		function initMap() {
+		  map = new google.maps.Map(document.getElementById('map'), {
+		    center: {lat: -34.397, lng: 150.644},
+		    zoom: 8
+		  });
+		}
+
+
+	// Counter
+
+	$('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+});
